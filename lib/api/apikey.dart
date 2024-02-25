344452f6d8f7a3dab25a6cb2573973b0fe0829cc
sammingasainath <105980835+sammingasainath@users.noreply.github.com>
2024-02-25 20:58:54 +0530

    Update apikey.dart

--- lib/api/apikey.dart
+++ lib/api/apikey.dart
@@ -1,3 +1,1 @@
-const api_key = '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';
-const mapbox_key =
-    'pk.eyJ1Ijoic2FtbWluZ2FzMSIsImEiOiJjbHNicHpjbGEwYnpmMnZsNWRsbzZ1b25qIn0.q03Rgt2P9GjJb6utXNU8LQ';
+
